a35578606724c18c8416a22193660806ee29555c
Karan Kadam <karank@outlook.com>
2023-07-30 19:43:26 +0930

    feat: 🎸 add logger and config

--- domain.py
+++ domain.py
@@ -1,23 +1,55 @@
+import logging
 import time
+import yaml
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+APP_CONFIG = read_config()
+LOGGER = setup_logger()
 
-def get_property_history_screenshot():
+# function to read config.yml file
+def read_config():
+    with open('config.yml') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+    return config
+
+
+def setup_logger():
+    # create logger
+    logger = logging.getLogger('domain')
+    logger.setLevel(logging.DEBUG)
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    # create formatter
+    formatter = logging.Formatter(
+        '%(asctime)s - %(levelname)s - %(message)s')
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(ch)
+
+    return logger
+
+def get_property_history_screenshot(property_address: str):
     chrome_options = Options()
-    chrome_options.binary_location = r"/opt/google/chrome/chrome"
+    chrome_options.binary_location = APP_CONFIG['chrome_path'] or "/opt/google/chrome/chrome"
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=chrome_options)
 
-    driver.get('https://www.domain.com.au/property-profile')
+    driver.get(APP_CONFIG['domain_url'])
     time.sleep(5)
 
     search_box = driver.find_element(By.CSS_SELECTOR, 'input[type="search"]')
-    search_box.send_keys('6 Lecornu Street, Broadview, SA 5083')
+    search_box.send_keys(property_address)
     time.sleep(2)
     search_box.send_keys(Keys.ENTER)
     time.sleep(5)
@@ -34,6 +66,8 @@ def get_property_history_screenshot():
     driver.quit()
 
 if __name__ == '__main__':
+    LOGGER.info('starting property helper bot...')
+
     get_property_history_screenshot()
     print('done creating screenshot')
     time.sleep(300)
